a9d6eb2cec4e8a9a51ff77d58f6e65e54a681a55
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-25 14:54:56 -0500

    pending list_spec tests in place

--- list_spec.rb
+++ list_spec.rb
@@ -3,5 +3,50 @@ require "rspec"
 require_relative "list"
 
 describe List do
-  # Your specs here
+
+  before do
+    task = mock(:task)
+    task.stub(:description => "Walk the dog") #.and_return("Walk the dog")
+  end
+
+  describe "#initialize" do
+    it "takes a title for its first argument" do
+      pending
+    end
+
+    it "requires one argument" do
+      pending
+    end
+  end
+
+  describe "#add_task" do
+    it "increments the tasks array by one task" do
+      pending
+    end
+  end
+
+  describe "#complete_task" do
+    it "marks a task as complete" do
+      pending
+    end
+  end
+
+  describe "#delete_task" do
+    it "decrements the tasks array by one task" do
+      pending
+    end
+  end
+
+  describe "#completed_tasks" do
+    it "returns an array of completed tasks" do
+      pending
+    end
+  end
+
+    describe "#incomplete_tasks" do
+    it "returns an array of incomplete tasks" do
+      pending
+    end
+  end
+
 end
